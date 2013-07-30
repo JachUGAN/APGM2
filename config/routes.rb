f625e6beb371c4cd70a4098eb40e30158499b591
Jach <jachugan@gmail.com>
2013-07-30 20:56:47 +0000

    1st commit 30July

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ APGM2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+root 'posts#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
