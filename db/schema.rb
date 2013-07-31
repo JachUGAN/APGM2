b6d07f5d78e12c8cc7ca6272f1dc27d935a9080e
Jach <jachugan@gmail.com>
2013-07-31 14:22:27 +0000

    1st commit 31July

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130730211927) do
+ActiveRecord::Schema.define(version: 20130731141816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "role"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.string   "rank"
+    t.string   "city"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
