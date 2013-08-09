0038e39bcdf2f9f92fda10fbfb44769b1016814a
Jach <jachugan@gmail.com>
2013-08-09 16:36:54 +0000

    3rd commit 09Aug

--- db/migrate/20130807215611_remove_password_digest_remember_token_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemovePasswordDigestRememberTokenFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :password_digest, :string
-    remove_column :users, :remember_token, :string
-  end
-end
